a2e484f1f9425b4fcbe1269deb9c80d24ca966a3
Sebastian Spier <spier.sebastian@gmail.com>
2010-10-17 00:24:47 +0200

    ver. 0.1

--- server.rb
+++ server.rb
@@ -3,6 +3,18 @@
 require 'rubygems'
 require 'sinatra'
 
+MDG = [
+  "Eradicate extreme poverty and hunger",
+  "Achieve universal primary education",
+  "Promote gender equality and empower women",
+  "Reduce child mortality",
+  "Improve maternal health",
+  "Combat HIV/AIDS, malaria, and other diseases",
+  "Ensure environmental sustainability",
+  "Develop a global partnership for development"
+]
+
+
 get '/' do
   erb :index
 end
