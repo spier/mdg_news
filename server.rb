9dbc5c76a363055dc716d68db3be38e32c9cffa7
Sebastian Spier <spier.sebastian@gmail.com>
2010-10-17 01:00:47 +0200

    all goals added

--- server.rb
+++ server.rb
@@ -3,17 +3,16 @@
 require 'rubygems'
 require 'sinatra'
 
-MDG = [
-  "Eradicate extreme poverty and hunger",
-  "Achieve universal primary education",
-  "Promote gender equality and empower women",
-  "Reduce child mortality",
-  "Improve maternal health",
-  "Combat HIV/AIDS, malaria, and other diseases",
-  "Ensure environmental sustainability",
-  "Develop a global partnership for development"
-]
-
+# MDG = [
+#   "Eradicate extreme poverty and hunger",
+#   "Achieve universal primary education",
+#   "Promote gender equality and empower women",
+#   "Reduce child mortality",
+#   "Improve maternal health",
+#   "Combat HIV/AIDS, malaria, and other diseases",
+#   "Ensure environmental sustainability",
+#   "Develop a global partnership for development"
+# ]
 
 get '/' do
   erb :index
