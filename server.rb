bc5d6f2ac96049e690344a35d96f13cfaf73ab18
spier <blog@airness.de>
2010-10-23 16:17:48 +0200

    adding getsatisfaction widget

--- server.rb
+++ server.rb
@@ -3,18 +3,6 @@
 require 'rubygems'
 require 'sinatra'
 
-# MDG = [
-#   "Eradicate extreme poverty and hunger",
-#   "Achieve universal primary education",
-#   "Promote gender equality and empower women",
-#   "Reduce child mortality",
-#   "Improve maternal health",
-#   "Combat HIV/AIDS, malaria, and other diseases",
-#   "Ensure environmental sustainability",
-#   "Develop a global partnership for development"
-# ]
-
 get '/' do
-  # @country = params[:country]
   erb :index
 end
